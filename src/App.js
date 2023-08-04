292b8e5df8e9aca295e42a871e661ccb8fb0918f
Chelappan <93577258+ChelappanKumar@users.noreply.github.com>
2023-08-04 12:24:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Going to Learn React
         </a>
       </header>
     </div>
